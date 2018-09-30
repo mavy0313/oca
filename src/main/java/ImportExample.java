42e3c4fd9c7374fde25c1858f03522957a9941aa
mavy0313 <vypoff@gmail.com>
2018-09-30 21:40:33 +0400

    Conflicts Import Example

---
+++ src/main/java/ImportExample.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ImportExample {
+    public static void main(String[] args) {
+        Random r = new Random(); // DOES NOT COMPILE
+        System.out.println(r.nextInt(10));
+    }
+}
